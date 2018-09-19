e4c867d10f7dce6b0281a96a786a5dff8e8aa018
EC2 Default User <ec2-user@ip-172-31-25-72.us-east-2.compute.internal>
2018-09-19 18:23:01 +0000

    CSS updates and new JS file

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction(event){
+            var w = window.open("", null, "height=500,width=500");
+            w.document.body.style.marginLeft = "20%";
+            w.document.body.style.backgroundColor = "#FFF391";
+            w.document.body.style.color = "black";
+            w.document.body.innerHTML = "Facts about this city";
+}
